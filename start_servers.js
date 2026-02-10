dd11e567d18a5dcaece60c5af0f7b510d94fe68c
rnayab755-creator <rnayab755@gmail.com>
2026-02-10 12:50:38 +0530

    Update start_servers.js

--- start_servers.js
+++ start_servers.js
@@ -4,6 +4,11 @@ const path = require('path');
 const startPort = 4000;
 const serverCount = 11; // 1 original + 10 additional
 
+app.get("/", (req, res) => {
+  res.send("Server Overload Queuing System API is running 🚀");
+});
+
+
 for (let i = 0; i < serverCount; i++) {
     const port = startPort + i;
     const proc = spawn('node', ['server.js', port], {
@@ -18,3 +23,4 @@ for (let i = 0; i < serverCount; i++) {
 
     console.log(`Spawning server on port ${port}...`);
 }
+
